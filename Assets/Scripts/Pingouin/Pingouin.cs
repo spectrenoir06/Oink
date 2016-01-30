89fc28daa858fd12c269f8d50037d34054dc80e0
bourreP <pierre.moltess@gmail.com>
2016-01-30 17:18:17 +0100

    Camera

--- Assets/Scripts/Pingouin/Pingouin.cs
+++ Assets/Scripts/Pingouin/Pingouin.cs
@@ -3,14 +3,16 @@ using System.Collections;
 
 public class Pingouin : MonoBehaviour
 {
-	// Use this for initialization
-	void Start ()
-    {
-        transform.LookAt(GameObject.FindGameObjectWithTag("Banquise").transform);
-    }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+  // Use this for initialization
+  void Start()
+  {
+    transform.LookAt(GameObject.FindGameObjectWithTag("Banquise").transform);
+    FishManager.Instance.createPrefabAtPostion(transform.position);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
 }
