d4ec6f2317edda763bbb591b517b69428c09d3aa
Vincent Studer <Vincent Studer>
2016-01-30 09:52:19 +0100

    rangement

---
+++ Assets/Scripts/Pingouin/IA/PingouinAIState.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PingouinAIState : NPCAIState
+{
+    PingouinNavigation navigation;
+
+    public PingouinAIState(NPCAIController controller, Transform transform, LuaEnvironnement luaEnvironnement, TextAsset script):
+        base(controller, transform, luaEnvironnement, script)
+    {
+        navigation = transform.GetComponent<PingouinNavigation>();
+    }
+
+    public void goToClosestFish()
+    {
+        Vector3 goal = FishManager.Instance.getClosestFishPosition(transform.position);
+        navigation.setGoalPosition(goal);
+        navigation.setWalkingState(true);
+    }
+
+    public void leaveAndWiggle()
+    {
+        
+    }
+
+    public void leave()
+    {
+
+    }
+
+    public void stopWalking()
+    {
+        navigation.setWalkingState(false);
+    }
+
+    public void startWalking()
+    {
+        navigation.setWalkingState(true);
+    }
+}
