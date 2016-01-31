6aad38a1e1d2b4accfa93d765f1cc37762cc1c6c
bourreP <pierre.moltess@gmail.com>
2016-01-31 03:35:33 +0100

    On ne peut pas changer d'animation si on est pas en state Walk

--- Assets/Scripts/Pingouin/PinguinAnimator.cs
+++ Assets/Scripts/Pingouin/PinguinAnimator.cs
@@ -1,83 +1,88 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PinguinAnimator : MonoBehaviour {
-  private enum StateAnimationPinguin
-  {
-    Walk,
-    Throw,
-    Dive,
-    Eat,
-    DanceThenDive,
-    DanceThenEat
-  }
-
-  public enum newStateAnimationPinguin
-  {
-    Throw,
-    Dive,
-    Eat,
-    DanceThenDive,
-    DanceThenEat
-  }
-
-  [SerializeField]
-  private NPCAIController iaController;
-
-  [SerializeField]
-  private Animator animator;
-
-  private StateAnimationPinguin currentState;
-
-  // Use this for initialization
-  void Start () {
-    currentState = StateAnimationPinguin.Walk;
-  }
-	
-	// Update is called once per frame
-	void Update () {
-  }
-
-  public void playAnimation(newStateAnimationPinguin state)
-  {
-    switch (state)
+public class PinguinAnimator : MonoBehaviour
+{
+    private enum StateAnimationPinguin
     {
-      case newStateAnimationPinguin.Throw:
-        animator.SetTrigger("Throw");
-        currentState = StateAnimationPinguin.Throw;
-        break;
-      case newStateAnimationPinguin.Dive:
-        animator.SetTrigger("Dive");
-        currentState = StateAnimationPinguin.Dive;
-        break;
-      case newStateAnimationPinguin.Eat:
-        animator.SetTrigger("Eat");
-        currentState = StateAnimationPinguin.Eat;
-        break;
-      case newStateAnimationPinguin.DanceThenDive:
-        animator.SetTrigger("Dance");
-        animator.SetTrigger("DiveAfterDance");
-        currentState = StateAnimationPinguin.DanceThenDive;
-        break;
-      case newStateAnimationPinguin.DanceThenEat:
-        animator.SetTrigger("Dance");
-        animator.SetTrigger("EatAfterDance");
-        currentState = StateAnimationPinguin.DanceThenEat;
-        break;
+        Walk,
+        Throw,
+        Dive,
+        Eat,
+        DanceThenDive,
+        DanceThenEat
     }
-  }
 
-  public void Destroy()
-  {
-    Destroy(transform.parent.parent.gameObject);
-  }
-
-  public void OnStartWalk()
-  {
-    if (currentState != StateAnimationPinguin.Walk)
+    public enum newStateAnimationPinguin
     {
-      iaController.startWalking();
-      currentState = StateAnimationPinguin.Walk;
+        Throw,
+        Dive,
+        Eat,
+        DanceThenDive,
+        DanceThenEat
     }
-  }
+
+    [SerializeField]
+    private NPCAIController iaController;
+
+    [SerializeField]
+    private Animator animator;
+
+    private StateAnimationPinguin currentState;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentState = StateAnimationPinguin.Walk;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void playAnimation(newStateAnimationPinguin state)
+    {
+        if (currentState != StateAnimationPinguin.Walk)
+            return;
+        switch (state)
+        {
+            case newStateAnimationPinguin.Throw:
+                animator.SetTrigger("Throw");
+                currentState = StateAnimationPinguin.Throw;
+                break;
+            case newStateAnimationPinguin.Dive:
+                animator.SetTrigger("Dive");
+                currentState = StateAnimationPinguin.Dive;
+                break;
+            case newStateAnimationPinguin.Eat:
+                animator.SetTrigger("Eat");
+                currentState = StateAnimationPinguin.Eat;
+                break;
+            case newStateAnimationPinguin.DanceThenDive:
+                animator.SetTrigger("DiveAfterDance");
+                animator.SetTrigger("Dance");
+                currentState = StateAnimationPinguin.DanceThenDive;
+                break;
+            case newStateAnimationPinguin.DanceThenEat:
+                animator.SetTrigger("EatAfterDance");
+                animator.SetTrigger("Dance");
+                currentState = StateAnimationPinguin.DanceThenEat;
+                break;
+        }
+    }
+
+    public void Destroy()
+    {
+        Destroy(transform.parent.gameObject);
+    }
+
+    public void OnStartWalk()
+    {
+        if (currentState != StateAnimationPinguin.Walk)
+        {
+            iaController.startWalking();
+            currentState = StateAnimationPinguin.Walk;
+        }
+    }
 }
