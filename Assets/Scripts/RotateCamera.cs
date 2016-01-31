d2faf93827c91224664823bf741793f72cbad4c4
bourreP <pierre.moltess@gmail.com>
2016-01-31 01:03:59 +0100

    Rotation de la camera

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -4,16 +4,20 @@ using System.Collections;
 public class RotateCamera : MonoBehaviour {
 
     [SerializeField]
-    private int speed = 5;
+    private int maxSpeed = 5;
 
     [SerializeField]
     private int border = 50;
 
     // Update is called once per frame
     void Update () {
+        float speed = 0;
         if (Input.mousePosition.x > Screen.width - border)
-            gameObject.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.eulerAngles - Vector3.up * speed);
+            speed = (Screen.width - border - Input.mousePosition.x) / (border) * maxSpeed;
         else if (Input.mousePosition.x < border)
-            gameObject.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.eulerAngles + Vector3.up * speed);
+            speed = (border - Input.mousePosition.x) / (border) * maxSpeed;
+
+        gameObject.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.eulerAngles + Vector3.up * speed);
+
     }
 }
